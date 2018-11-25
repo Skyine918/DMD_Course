ebd042b3e565a997e1a0e618b351bf7273a706a4
Skyline918 <twicetwix@gmail.com>
2018-11-26 02:09:43 +0300

    DB file updated

--- src/DBFiller.java
+++ src/DBFiller.java
@@ -17,7 +17,7 @@ public class DBFiller {
         LinkedList<String> sqls = new LinkedList();
 
         // SQL statement for creating a new table
-        sqls.add("CREATE TABLE IF NOT EXISTS customer (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Customers (\n"
                 + "	username VARCHAR(30) PRIMARY KEY,\n"
                 + "	fullname VARCHAR(50),\n"
                 + "	email VARCHAR(320),\n"
@@ -27,28 +27,28 @@ public class DBFiller {
                 + " zipcode VARCHAR(30)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS charging_station (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Charging_stations (\n"
                 + " CSID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " sockets_count INTEGER,\n"
-                + " price_for_charge_unit INTEGER,\n"
+                + " price_for_minute INTEGER,\n"
                 + " location VARCHAR(100)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS sockets (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Sockets (\n"
                 + " SID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " CSID INTEGER,\n"
+                + " socket VARCHAR(100),\n"
                 + " total_amount INTEGER,\n"
                 + " available_amount INTEGER,\n"
-                + " socket VARCHAR(100),\n"
-                + " FOREIGN KEY (CSID) REFERENCES charging_station(CSID)\n"
+                + " FOREIGN KEY (CSID) REFERENCES Charging_stations(CSID)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS car (\n"
-                + " PLATE VARCHAR(50) PRIMARY KEY,\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Cars (\n"
+                + " plate VARCHAR(50) PRIMARY KEY,\n"
                 + " location VARCHAR(100),\n"
                 + "	model VARCHAR(50) NOT NULL,\n"
                 + "	color VARCHAR(30),\n"
-                + " charge INTEGER,\n"
+                + " charge_level INTEGER,\n"
                 + " plug VARCHAR(50),\n"
                 + " time_of_charging INTEGER,\n"
                 + " km_per_percent INTEGER,\n"
@@ -58,98 +58,100 @@ public class DBFiller {
                 + " TotalDistance INTEGER\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS orders (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Orders (\n"
                 + " OID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " username VARCHAR(30),\n"
-                + " PLATE VARCHAR(50),\n"
-                + "	datetime smalldatetime,\n"
+                + " plate VARCHAR(50),\n"
+                + "	date date,\n"
+                + " time time,\n"
                 + " color VARCHAR(15),\n"
                 + " model VARCHAR(50),\n"
-                + " capacity INTEGER,\n"
                 + " init_address VARCHAR(50),\n"
                 + " arriving_address VARCHAR(50),\n"
                 + " IsPaid BIT,\n"
                 + " CardData VARCHAR(50),\n"
-                + " FOREIGN KEY (username) REFERENCES customer(username),\n"
-                + " FOREIGN KEY (PLATE) REFERENCES car(PLATE)\n"
+                + " FOREIGN KEY (username) REFERENCES Customers(username),\n"
+                + " FOREIGN KEY (plate) REFERENCES Cars(plate)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS provider (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Providers (\n"
                 + " PID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                + " model VARCHAR(100),\n"
                 + " address VARCHAR(50),\n"
                 + " phone VARCHAR(15)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS workshop (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Workshops (\n"
                 + " WID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " location VARCHAR(50),\n"
                 + " availability BIT\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS trip (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Trips (\n"
                 + "	TID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + "	OID INTEGER,\n"
                 + "	starttime smalldatetime NOT NULL,\n"
                 + "	finishtime smalldatetime NOT NULL,\n"
                 + "	distance_to_init_address INTEGER NOT NULL,\n"
                 + "	total_distance INTEGER NOT NULL,\n"
-                + " FOREIGN KEY (OID) REFERENCES orders(OID)\n"
+                + " FOREIGN KEY (OID) REFERENCES Orders(OID)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS repair (\n"
-                + "	RID INTEGER PRIMARY KEY,\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Repairs (\n"
+                + "	RID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + "	ProviderID INTEGER,\n"
-                + "	PLATE VARCHAR(50),\n"
+                + "	plate VARCHAR(50),\n"
                 + "	WorkshopID INTEGER,\n"
                 + "	parts VARCHAR(100),\n"
                 + "	price INTEGER,\n"
-                + "	datetime smalldatetime NOT NULL,\n"
-                + " FOREIGN KEY (ProviderID) REFERENCES provider(PID),\n"
-                + " FOREIGN KEY (PLATE) REFERENCES car(PLATE),\n"
-                + " FOREIGN KEY (WorkshopID) REFERENCES workshop(WID)\n"
+                + "	date date,\n"
+                + " time time,\n"
+                + " FOREIGN KEY (ProviderID) REFERENCES Providers(PID),\n"
+                + " FOREIGN KEY (plate) REFERENCES Cars(plate),\n"
+                + " FOREIGN KEY (WorkshopID) REFERENCES Workshops(WID)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS charge (\n"
-                + " PLATE VARCHAR(50),\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Charges (\n"
+                + "	CID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                + " plate VARCHAR(50),\n"
                 + " CSID INTEGER,\n"
                 + "	time smalldatetime,\n"
                 + " Price INTEGER,\n"
-                + " FOREIGN KEY (PLATE) REFERENCES car(PLATE),\n"
-                + " FOREIGN KEY (CSID) REFERENCES charging_station(CSID)\n"
+                + " FOREIGN KEY (plate) REFERENCES Cars(plate),\n"
+                + " FOREIGN KEY (CSID) REFERENCES Charging_stations(CSID)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS provider_parts (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Provider_parts (\n"
                 + "	PPID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " PID INTEGER,\n"
-                + " model VARCHAR(100),\n"
                 + " part VARCHAR(100),\n"
                 + " amount INTEGER,\n"
-                + " FOREIGN KEY (PID) REFERENCES provider(RID)\n"
+                + " FOREIGN KEY (PID) REFERENCES Providers(RID)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS workshop_parts (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Workshop_parts (\n"
                 + "	WPID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " WID INTEGER,\n"
                 + " model VARCHAR(100),\n"
                 + " part VARCHAR(100),\n"
                 + " amount INTEGER,\n"
-                + " FOREIGN KEY (WID) REFERENCES workshop(WID)\n"
+                + " FOREIGN KEY (WID) REFERENCES Workshops(WID)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS error (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Logs (\n"
                 + "	ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " ExternalID INTEGER,\n"
                 + " TableName INTEGER,\n"
                 + " ErrorMessage VARCHAR(100)\n"
                 + ");");
 
-        sqls.add("CREATE TABLE IF NOT EXISTS payments (\n"
+        sqls.add("CREATE TABLE IF NOT EXISTS Payments (\n"
                 + "	PID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                 + " OID INTEGER,\n"
                 + " amount INTEGER,\n"
                 + " carddata VARCHAR(100),\n"
                 + " payment_time smalldatetime,\n"
-                + " FOREIGN KEY (OID) REFERENCES orders(OID)\n"
+                + " FOREIGN KEY (OID) REFERENCES Orders(OID)\n"
                 + ");");
 
         try (Connection conn = DriverManager.getConnection(url);
@@ -171,20 +173,20 @@ public class DBFiller {
         LinkedList<String> sqls = new LinkedList();
 
         // SQL statement for creating a new table
-        sqls.add("DROP TABLE IF EXISTS customer;");
-        sqls.add("DROP TABLE IF EXISTS payments;");
-        sqls.add("DROP TABLE IF EXISTS sockets;");
-        sqls.add("DROP TABLE IF EXISTS car;");
-        sqls.add("DROP TABLE IF EXISTS orders;");
-        sqls.add("DROP TABLE IF EXISTS provider;");
-        sqls.add("DROP TABLE IF EXISTS workshop;");
-        sqls.add("DROP TABLE IF EXISTS charging_station;");
-        sqls.add("DROP TABLE IF EXISTS trip;");
-        sqls.add("DROP TABLE IF EXISTS repair;");
-        sqls.add("DROP TABLE IF EXISTS charge;");
-        sqls.add("DROP TABLE IF EXISTS workshop_parts;");
-        sqls.add("DROP TABLE IF EXISTS provider_parts;");
-        sqls.add("DROP TABLE IF EXISTS error;");
+        sqls.add("DROP TABLE IF EXISTS Customers;");
+        sqls.add("DROP TABLE IF EXISTS Payments;");
+        sqls.add("DROP TABLE IF EXISTS Sockets;");
+        sqls.add("DROP TABLE IF EXISTS Cars;");
+        sqls.add("DROP TABLE IF EXISTS Orders;");
+        sqls.add("DROP TABLE IF EXISTS Providers;");
+        sqls.add("DROP TABLE IF EXISTS Workshops;");
+        sqls.add("DROP TABLE IF EXISTS Charging_stations;");
+        sqls.add("DROP TABLE IF EXISTS Trips;");
+        sqls.add("DROP TABLE IF EXISTS Repairs;");
+        sqls.add("DROP TABLE IF EXISTS Charges;");
+        sqls.add("DROP TABLE IF EXISTS Workshop_parts;");
+        sqls.add("DROP TABLE IF EXISTS Provider_parts;");
+        sqls.add("DROP TABLE IF EXISTS Logs;");
 
 
         try (Connection conn = DriverManager.getConnection(url);
@@ -206,7 +208,7 @@ public class DBFiller {
         LinkedList<String> sqls = new LinkedList();
 
         // SQL statement for creating a new table
-        sqls.add("INSERT INTO car (PLATE, model, color, location) VALUES" +
+        sqls.add("INSERT INTO Cars (plate, model, color, location) VALUES" +
                 " ('AA100A', 'Tesla Xyesla', 'Black', 'Street 1'),\n" +
                 " ('AA100B', 'Lada Kalina', 'Black', 'Street 1'),\n" +
                 " ('AA100C', 'Toyota KEK', 'White', 'Street 2'),\n" +
@@ -219,33 +221,33 @@ public class DBFiller {
 
                 ";");
 
-        sqls.add("INSERT INTO orders (username, PLATE, datetime) VALUES" +
-                " ('user1', 'AA100A', '25/11/2017 12:30'),\n" +
-                " ('user1', 'AA100A', '25/11/2017 12:30'),\n" +
-                " ('user1', 'AA100A', '25/11/2017 12:30'),\n" +
-                " ('user1', 'AA100A', '25/11/2017 12:30'),\n" +
-                " ('user2', 'AA100A', '25/11/2016 12:30'),\n" +
-                " ('user2', 'AA100A', '25/11/2015 12:30'),\n" +
-                " ('user2', 'AA100A', '25/11/2018 12:30'),\n" +
-                " ('user3', 'AA100A', '25/11/2018 12:30'),\n" +
-                " ('user5', 'AA100A', '25/11/2018 12:30')\n" +
+        sqls.add("INSERT INTO Orders (username, plate, date, time) VALUES" +
+                " ('user1', 'AA100A', '25/11/2017', '12:30'),\n" +
+                " ('user1', 'AA100A', '25/11/2017', '12:30'),\n" +
+                " ('user1', 'AA100A', '25/11/2017', '12:30'),\n" +
+                " ('user1', 'AA100A', '25/11/2017', '12:30'),\n" +
+                " ('user2', 'AA100A', '25/11/2016', '12:30'),\n" +
+                " ('user2', 'AA100A', '25/11/2015', '12:30'),\n" +
+                " ('user2', 'AA100A', '25/11/2018', '12:30'),\n" +
+                " ('user3', 'AA100A', '25/11/2018', '12:30'),\n" +
+                " ('user5', 'AA100A', '25/11/2018', '12:30')\n" +
 
                 ";");
 
-        sqls.add("INSERT INTO payments (OID, amount, payment_time) VALUES" +
-                " ('1', '222', '25/11/2017 12:30'),\n" +
-                " ('2', '22', '25/11/2017 12:30'),\n" +
-                " ('3', '2', '25/11/2017 12:30'),\n" +
-                " ('4', '222', '25/11/2017 12:30'),\n" +
-                " ('5', '222', '25/11/2016 12:30'),\n" +
-                " ('5', '222', '25/11/2015 12:30'),\n" +
-                " ('5', '222', '25/11/2018 12:30'),\n" +
+        sqls.add("INSERT INTO Payments (OID, amount, payment_time) VALUES" +
+                " ('1', '222', '25/10/2018 12:30'),\n" +
+                " ('2', '22', '25/10/2018 12:30'),\n" +
+                " ('3', '2', '25/10/2018 12:30'),\n" +
+                " ('4', '222', '25/10/2018 12:30'),\n" +
+                " ('5', '222', '25/10/2018 12:30'),\n" +
+                " ('5', '222', '25/10/2018 12:30'),\n" +
+                " ('5', '222', '25/10/2018 12:30'),\n" +
                 " ('6', '222', '25/11/2018 12:30'),\n" +
                 " ('3', '222', '25/11/2018 12:30')\n" +
 
                 ";");
 
-        sqls.add("INSERT INTO customer (username) VALUES" +
+        sqls.add("INSERT INTO Customers (username) VALUES" +
                 " ('user1'),\n" +
                 " ('user2'),\n" +
                 " ('user3'),\n" +
@@ -274,7 +276,7 @@ public class DBFiller {
         LinkedList<String> sqls = new LinkedList();
 
         // SQL statement for creating a new table
-        sqls.add("SELECT PLATE FROM car" +
+        sqls.add("SELECT plate FROM Cars" +
                 " JOIN trip." +
                 " ON trip.OID = orders.OID\n" +
                 " JOIN order ON order.OID = trip.OID\n");
